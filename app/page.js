527e5cd444a8225a746d98a8a391a386888c8f28
ayaz07 <118646687+Ayaz55555@users.noreply.github.com>
2025-12-01 19:39:57 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
     <main className="p-4 max-w-3xl mx-auto space-y-6">
       <Header />
       <HeroCard />
-      <ActionsGri />
+      <ActionsGrid />
     </main>
   );
 }
