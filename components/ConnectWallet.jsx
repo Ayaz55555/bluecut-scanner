5db8b626667dfca6c4ad7c08de9df02b46c29124
ayaz07 <118646687+Ayaz55555@users.noreply.github.com>
2026-01-12 17:57:44 +0530

    Update ConnectWallet.jsx

--- components/ConnectWallet.jsx
+++ components/ConnectWallet.jsx
@@ -3,7 +3,7 @@ import { appkit } from "@/lib/wallet";
 
 export default function ConnectWallet() {
   return (
-    <butto
+    <button
       onClick={() => appkit.open()}
       className="px-4 py-2 bg-blue-600 text-white rounded-xl hover:bg-blue-700"
     >
