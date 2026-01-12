c6c096ec8b67b80b9b7a73ff8124b231d99859df
ayaz07 <118646687+Ayaz55555@users.noreply.github.com>
2026-01-12 17:59:03 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        sourc: "/(.*)",
+        source: "/(.*)",
         headers: [
           {
             key: "Content-Security-Policy",
